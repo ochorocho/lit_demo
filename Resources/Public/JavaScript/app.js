5cfea793fc94bf3ef2fd11ec146571ec392ca267
Jochen <jochen.roth@b13.com>
2026-03-31 23:21:58 +0200

    Add panel styling for component

--- Resources/Public/JavaScript/app.js
+++ Resources/Public/JavaScript/app.js
@@ -4,4 +4,9 @@ import './component/counter.js';
 // https://api.open-meteo.com/v1/forecast?latitude=28.33&longitude=-14.01&hourly=temperature_2m
 
 // @todo: Query gitlab.com using GraphQL:
-// ...
+// 1) Fetch your GitHub Users Repo https://api.github.com/users/{user}/repos?per_page=100&page=1
+// 2) Loop over the returned repos and collect the 'url'
+// 3) Loop over the collected repo urls and count your commits:
+//    https://api.github.com/repos/{user}/{repo}/commits?author={user}&per_page=100&page=1
+// 4) Dynamically display the data while its retrieved
+//    e.g. | repo path | commit count |
--- Resources/Public/JavaScript/component/counter.js
+++ Resources/Public/JavaScript/component/counter.js
@@ -3,94 +3,88 @@ import {lll} from '@typo3/core/lit-helper.js';
 
 export class Counter extends LitElement {
     static properties = {
-        counter: { type: Number, attribute: 'start' },
-        icon: { type: String, attribute: 'icon' },
-        _pattern: { type: Array, state: true },
+        start: { type: Number, attribute: 'start' },
+        _counter: { state: true },
+        _pattern: { state: true },
     };
 
+    // Define component specific CSS
     static styles = css`
         :host {
             --icon-color-primary: red;
-        }
-
-        @keyframes marquee-content {
-            from {
-                transform: translateX(0%);
-            }
-            to {
-                transform: translateX(+100%);
-            }
-        }
-        
-        .marquee {
-            display: flex;
-            overflow: hidden;
-            white-space: nowrap;
-        }
-
-        .marquee__item {
-            animation-duration: 4s;
-            animation-iteration-count: infinite;
-            animation-name: marquee-content;
-            animation-timing-function: linear;
         }
     `
 
     constructor() {
         super();
-        this.counter = 1;
-        this.icon = 'actions-info';
+        this.start = 0;
+        this._counter = 0;
         this._pattern = [];
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        // Initialize the internal state with the attribute start="X"
+        this._counter = this.start;
     }
 
     render() {
         return html`
-            <div>
-                <h1>
-                    <typo3-backend-icon identifier="lit-demo-logo" size="medium" aria-hidden="true"></typo3-backend-icon>
-                    ${lll('lit-demo.title')}
-                </h1>
-                <div>${lll('lit-demo.counter.label')}: ${this.counter}</div>
-                <div>
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-icon">
+                        <typo3-backend-icon identifier="lit-demo-logo" size="medium" aria-hidden="true"></typo3-backend-icon>
+                    </div>
+                    <div class="card-header-body">
+                        <h2 class="card-title">
+                            ${lll('lit-demo.title')}
+                        </h2>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        ${lll('lit-demo.counter.label')}: ${this._counter}
+
+                        <div class="marquee">
+                            <div class="marquee__item">
+                                ${this.vengaBus() ? '🚌🎶... The VengaBus is coming ... 🎶 🚌' : nothing}
+                            </div>
+                        </div>
+                    </p>
+                </div>
+                <div class="card-footer">
                     <button @click="${this.countUp}" class="btn btn-primary">
                         <typo3-backend-icon identifier="actions-arrow-up-alt" size="small" aria-hidden="true"></typo3-backend-icon>
                     </button>
                     <button @click="${this.countDown}" class="btn btn-secondary">
                         <typo3-backend-icon identifier="actions-arrow-down-alt" size="small" aria-hidden="true"></typo3-backend-icon>
                     </button>
-                    
-                    <div class="marquee">
-                        <div class="marquee__item">
-                            ${this.vengaBus() ? '🚌🎶... The VengaBus is coming ... 🎶 🚌' : nothing}
-                        </div>
-                    </div>
-                    
                 </div>
             </div>
         `;
     }
 
     countUp() {
-        this.counter++;
-        this._pattern.push(this.counter);
+        this._counter++;
+        this._pattern.push(this._counter);
     }
 
     countDown() {
-        this.counter--;
-        this._pattern.push(this.counter);
+        this._counter--;
+        this._pattern.push(this._counter);
     }
 
     vengaBus() {
-        let expectedPattern = [2,1,2,1,2,1,2,1]
+        let expectedPattern = [1,0,1,0,1,0,1,0]
         return this._pattern.length === expectedPattern.length && this._pattern.every((val, i) => val === expectedPattern[i]);
     }
 
     // Disable the shadow-root
     // - if disabled (uncommented), CSS from outside the component will be applied
     // - if enabled (remove the following code), only specific
-    // createRenderRoot() {
-    //     return this;
-    // }
+    createRenderRoot() {
+        return this;
+    }
 }
 
 customElements.define('ld-counter', Counter);
